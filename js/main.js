8f953e554be66bd33273369c874721e5ad5fbb4a
Kompbog <kompbog@gmail.com>
2016-06-25 20:57:05 +0300

    js>svgh100%
    Сделал .main высоту его бекграунда через js (w*(svgh/svgw)
    + media для bg
    // Есть маленький баг, когда ширина ~400 внизу появляется пустота,
    котора быстро проподает

--- js/main.js
+++ js/main.js
@@ -6,6 +6,21 @@
   } 
   scrinH(); 
   $(window).resize(scrinH); 
+  
+  
+  function conentH(){ 
+    var multiplier;
+    if($(window).width() < 384) {
+      multiplier = 7.673;
+    } else {
+      multiplier = 5.294;
+    }
+    
+    var mapH = $(window).width() * multiplier; 
+    $(".main").css("height", mapH +"px");
+  } 
+  conentH(); 
+  $(window).resize(conentH); 
 
 
 })(jQuery);
