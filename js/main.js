163152f74ed6fd504aa53bf1cc6cb6a4e79340b3
Kompbog <kompbog@gmail.com>
2016-06-28 10:38:40 +0300

    active btn station
    Блоки станций по центру но без анимации
    Через js поставил блоки стации посредите

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 (function ($) { 
-
-  function scrinH(){ 
+  
+  
+  function scrinM(){ 
+    var a = $('.station__btn').height();
+    var aa = a/2
+    var b = $(window).height();
+    var bb = b/2
+    var c = bb+aa
+    var d = c * -1
+    $(".scrin--bg").css("top", d +"px") 
+  } 
+  scrinM(); 
+  $(window).resize(scrinM); 
+  
+  function stationM(){ 
+    var a = $('.gallary').height();
+    
+    var b = $(window).height();
+    var c = (b-a)/4
+    $(".gallary").css("margin-top", c +"px") 
+  } 
+  stationM(); 
+  $(window).resize(stationM); 
+  
+  function scrinH(){
     var windowH = $(window).height(); 
     $(".scrin, #line").css("height", windowH +"px") 
   } 
