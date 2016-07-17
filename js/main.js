b31e08cabc2fc05d74a3563becdeba0530d45797
Kompbog <kompbog@gmail.com>
2016-07-17 14:46:58 +0300

    Refactoring(Jade, BEM, SMACSS) + js popup

--- js/main.js
+++ js/main.js
@@ -1,108 +1,70 @@
-(function ($) { 
-  
-//ВЫРАВНИВАНИЕ СКРИНА ПОСРЕДИНЕ
-  function scrinM(){ 
-    var a = $('.station__btn').height();
-    var aa = a/2;
-    var b = $(window).height();
-    var bb = b/2;
-    var c = bb+aa;
-    var d = c*-1;
-    $(".scrin--bg").css("top", d) 
-  }
-  scrinM(); 
-    $(window).resize(scrinM);
-  
-// КВАДРАТНЫЕ ОКОН СТАНЦИЙ
-  function gMap(){ 
-    var a = $('.map').width();
-    var b = $(window).height();
-    var c = $(window).width();
-    var d = $('.gallary__photo').height()*1.38805970149
-    if(a < b) {
-      $(".map").css("height", a +"px")
-      $(".gallary__description").css("height", d +"px")
-      $(".map").css("width", 100 +"%")
-    } else {
-      $(".map").css("width", b +"px")
-      $(".map").css("height", 100 +"%")
-    }  
-  } 
-  gMap(); 
-  $(window).resize(gMap);
-  
+(function ($) {
+
+//BTN
+  $('.dot__btn--shamShuiPo').on('click', function(){
+    $('.main').toggleClass('clicked');
+});
+$('.gallery__btn--map').on('click', function(){
+  $('.gallery__content').toggleClass('clickedMap');
+});
 //ЦЕНТРИРОВАНИЕ ПО ВЫСОТЕ
-  function gMapH(){ 
-    var aH = $('.gallary').height();
+  function gMapH(){
+    var aH = $('.gallery').height();
     var bH = $(window).height();
     var cH = (bH-aH)/2;
-    $(".gallary").css("top", cH +"px") 
-    $(".gallary").css("bottom", cH +"px")
-  } 
-  gMapH(); 
+    $(".popUp").css("padding-top", cH +"px");
+    $(".popUp").css("padding-bottom", cH +"px");
+  }
+  gMapH();
   $(window).resize(gMapH);
 
-//ЦЕНТРИРОВАНИЕ ПО ШИРИНЕ  
-//  function gMapW(){ 
-//    var aW = $('.gallary').width();
+//ЦЕНТРИРОВАНИЕ ПО ШИРИНЕ
+//  function gMapW(){
+//    var aW = $('.gallery').width();
 //    var bW = $(window).width();
 //    var cW = (bW-aW)/2;
-//    $(".gallary").css("left", cW +"px") 
-//    $(".gallary").css("right", cW +"px")
-//  } 
-//  gMapW(); 
+//    $(".gallery").css("left", cW +"px");
+//    $(".gallery").css("right", cW +"px");
+//  }
+//  gMapW();
 //  $(window).resize(gMapW);
-  
-  
-  stationM11(); 
-  $(window).resize(stationM11); 
-    function stationM11(){ 
-      var a = $('#g11').height();
-      var b = $(window).height();
-      var c = (b-a)/2
-      $("#g11").css("margin-top", c +"px") 
-      $("#g11").css("margin-bottom", c +"px")
-    } 
-  stationM11(); 
-  $(window).resize(stationM11); 
-  
-  
-  
 
-//ВЫСОТА ДЛЯ СКРИНА  
+
+//ВЫСОТА ДЛЯ СКРИНА
   function scrinH(){
-    var windowH = $(window).height(); 
-    $(".scrin, #line").css("height", windowH) 
-  } 
-  scrinH(); 
-  $(window).resize(scrinH); 
-  
-//ВЫСОТА ДЛЯ КАРТЫ СВГ  
-  function conentH(){ 
+    var windowH = $(window).height();
+    $(".hello, .info, .line, .radGrad").css("height", windowH);
+  }
+  scrinH();
+  $(window).resize(scrinH);
+
+//ВЫСОТА ДЛЯ КАРТЫ СВГ
+  function conentH(){
     var multiplier;
-    if($(window).width() < 384) {
+    if($(window).width() < 385) {
       multiplier = 7.673;
     } else {
       multiplier = 5.294;
     }
-    
-    var mapH = $(window).width() * multiplier; 
-    $(".main").css("height", mapH +"px");
-  } 
-  conentH(); 
-  $(window).resize(conentH); 
 
-  function shadowH(){ 
+    var mapH = $(window).width() * multiplier;
+    $(".main").css("height", mapH +"px");
+  }
+  conentH();
+  $(window).resize(conentH);
+
+//ВЫСОТА ТЕНИ В НАЧАЛЕ КАРТЫ
+  function shadowH(){
     var multiplier;
     if($(window).width() < 384) {
-      multiplier = 0.30692;
+      multiplier = 0.071176;
     } else {
-      multiplier = 0.21176;
+      multiplier = 0.080692;
     }
-    var mapShadowH = $("#main").height() * multiplier; 
-    $(".shadowMap").css("height", mapShadowH +"px") 
-  } 
-  shadowH(); 
+    var mapShadowH = $(".main").height() * multiplier;
+    $(".shadowMap").css("height", mapShadowH +"px");
+  }
+  shadowH();
   $(window).resize(shadowH);
 
 })(jQuery);
